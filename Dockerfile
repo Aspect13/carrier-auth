e253a2880379f563b1b2aa1e629ec06e33a05ca6
Aspect13 <ad13box@gmail.com>
2021-04-27 18:47:38 +0300

    Revert "Bugfix/group model attributes field type"

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,21 @@
-FROM python:3.8
+FROM python:3.7-alpine
+
+RUN apk add --update --no-cache \
+  build-base \
+  coreutils \
+  gcc \
+  libffi-dev \
+  libressl \
+  libressl-dev
 
 ENV PYTHONUNBUFFERED 1
-RUN pip install --upgrade pip
-RUN pip install --upgrade setuptools
+RUN pip3 install --upgrade pip
+RUN pip3 install --upgrade setuptools
 
-WORKDIR /app
+WORKDIR /auth
 COPY requirements.txt .
-RUN pip install -r requirements.txt --no-cache
+COPY auth /auth/auth
+COPY setup.py .
+RUN set -x && pip3 install --no-cache-dir /auth
 
-COPY plugins ./plugins
-COPY config/pylon.yml ./config/pylon.yml
-COPY config/*.yml ./config/
-COPY app.py .
-
-ENTRYPOINT ["python", "app.py"]
+ENTRYPOINT ["app"]
--- auth/utils/session.py
+++ auth/utils/session.py
@@ -12,23 +12,13 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-from setuptools import setup, find_packages
 
-with open("requirements.txt") as f:
-    required_dependencies = f.read().splitlines()
-
-setup(
-    name="auth",
-    version="0.0.1",
-    license="Apache License 2.0",
-    author="Carrier team",
-    author_email="artem_rozumenko@epam.com",
-    url="https://getcarrier.io",
-    description="Auth middleware",
-    long_description="Auth middleware",
-    packages=find_packages(),
-    zip_safe=False,
-    include_package_data=True,
-    install_requires=required_dependencies,
-    entry_points={"console_scripts": ["app = auth.app:main"]},
-)
+def clear_session(session):
+    session["name"] = "auth"
+    session["state"] = ""
+    session["nonce"] = ""
+    session["auth"] = False
+    session["auth_errors"] = []
+    session["auth_nameid"] = ""
+    session["auth_sessionindex"] = ""
+    session["auth_attributes"] = ""
